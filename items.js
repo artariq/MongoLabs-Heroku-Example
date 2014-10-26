1b2a9eb1d4606e21f5102790b0f8b0f3d810f222
Ali Rushdan Tariq <alirtariq@gmail.com>
2014-10-25 23:35:53 -0400

    mongolab example with heroku

---
+++ items.js
@@ -0,0 +1,67 @@
+var mongoose = require('mongoose');
+
+var itemSchema = mongoose.Schema({
+    item: String
+});
+
+var Item = mongoose.model('Item', itemSchema);
+
+var items = [];
+
+exports.create = function(req, res){
+	if (!req.param('item')) {
+		res.send('Item not valid');
+		res.statusCode = 400;
+	} else {
+		items.push(req.param('item'));
+		res.send("adding " + req.param('item'));
+	}
+};
+
+exports.retrieveAll = function(req, res){
+	items.forEach(function (item, i) {
+		res.write(i + '. ' + item + '\n');
+	});
+	res.end();
+};
+
+exports.retrieveOne = function(req, res) {
+	if (isNaN(req.params.id)) {
+		res.send('Item id not valid');
+		res.statusCode = 400;
+	}
+	else if (items[req.params.id]) {
+		res.send("getting "+req.params.id);
+	}
+	else {
+		res.send("item not found");
+	}
+}
+
+exports.update = function(req, res) {
+	if (isNaN(req.params.id)) {
+		res.send('Item id not valid');
+		res.statusCode = 400;
+	}
+	else if (items[req.params.id] && req.param('item')) {
+		items[req.params.id] = req.param('item');
+		res.send("putting "+req.params.id);
+	}
+	else {
+		res.send("item not found");
+	}
+}
+
+exports.delete = function(req, res) {
+	if (isNaN(req.params.id)) {
+		res.send('Item id not valid');
+		res.statusCode = 400;
+	}
+	else if (items[req.params.id]) {
+		items.splice(req.params.id, 1);
+		res.end('Item deleted successfully');
+	}
+	else {
+		res.send("item not found");
+	}
+}
--- server.js
+++ server.js
@@ -1,80 +1,42 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var myitems = require('./items');
 
-var items = [];
+var mongoose = require('mongoose');
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use( bodyParser.urlencoded() ); // to support URL-encoded bodies
-
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
 // gets all items, ie http GET localhost:5000
-app.get('/', function(req, res){
-	items.forEach(function (item, i) {
-		res.write(i + '. ' + item + '\n');
-	});
-	res.end();
-});
+app.get('/', myitems.retrieveAll);
 
-// gets a single item, ie the second item http GET localhost:5000/1
-app.get('/:index', function(req, res){
-	if (isNaN(req.params.index)) {
-		res.send('Item id not valid');
-		res.statusCode = 400;
-	}
-	else if (items[req.params.index]) {
-		res.send("getting "+req.params.index);
-	}
-	else {
-res.send("item not found");
-}
-});
+// gets a single item, ie the second item http GET localhost:5000/id
+app.get('/:id', myitems.retrieveOne);
 
 // adds and item, ie http --form POST localhost:5000 item='oranges'
-app.post('/', function(req, res){
-	if (!req.param('item')) {
-		res.send('Item not valid');
-		res.statusCode = 400;
-	} else {
-		items.push(req.param('item'));
-		res.send("adding " + req.param('item'));
-	}
-});
+app.post('/', myitems.create);
 
-// updates an item, ie http --form PUT localhost:5000/1 item='chocolate'
-app.put('/:index', function(req, res){
-	if (isNaN(req.params.index)) {
-		res.send('Item id not valid');
-		res.statusCode = 400;
-	}
-	else if (items[req.params.index] && req.param('item')) {
-		items[req.params.index] = req.param('item');
-		res.send("putting "+req.params.index);
-	}
-	else {
-		res.send("item not found");
-	}
-});
+// updates an item, ie http --form PUT localhost:5000/id item='chocolate'
+app.put('/:id', myitems.update);
 
-// deletes an item, ie http DELETE localhost:5000/1
-app.delete('/:index', function(req, res){
-	if (isNaN(req.params.index)) {
-		res.send('Item id not valid');
-		res.statusCode = 400;
-	}
-	else if (items[req.params.index]) {
-		items.splice(req.params.index, 1);
-		res.end('Item deleted successfully');
-	}
-	else {
-		res.send("item not found");
-	}
-});
+// deletes an item, ie http DELETE localhost:5000/id
+app.delete('/:id', myitems.delete);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
 	console.log('listening on '+port);
 });
+
+mongoose.connect('mongodb://artariq:chitral@ds047040.mongolab.com:47040/rushiesmongodb');
+
+var db = mongoose.connection;
+db.on('error', function callback () {
+    console.error('connection error');
+});
+db.once('open', function callback () {
+    console.error('connection success');
+});
+
